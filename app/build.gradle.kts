9dcf24f3c1bd799235a453b4effcbb3b8df97e7b
AWAiS <5278488+AwaisKing@users.noreply.github.com>
2024-01-23 01:00:00 +0500

    my own files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import java.util.Properties
-import java.io.FileInputStream
 import com.android.build.gradle.internal.api.BaseVariantOutputImpl
 
 plugins {
@@ -13,40 +11,23 @@ android {
 
     defaultConfig {
         applicationId = "com.apkupdater"
-        minSdk = 21
+
+        minSdk = 24
         targetSdk = 34
+
         versionCode = 51
         versionName = "3.0.2"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables.useSupportLibrary = true
-    }
 
-    signingConfigs {
-        create("release") {
-            try {
-                val props = Properties()
-                props.load(FileInputStream(file("../local.properties")))
-                storeFile = file(props.getProperty("keystore.file"))
-                storePassword = props.getProperty("keystore.password")
-                keyAlias = props.getProperty("keystore.keyalias")
-                keyPassword = props.getProperty("keystore.keypassword")
-            } catch (ignored: Exception) {
-                val config = signingConfigs.getByName("debug")
-                storeFile = config.storeFile
-                storePassword = config.storePassword
-                keyAlias = config.keyAlias
-                keyPassword = config.keyPassword
-                println("Signing config not found. Using debug settings.")
-            }
-            enableV3Signing = true
-        }
+        vectorDrawables.useSupportLibrary = true
     }
 
     buildTypes {
         getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
@@ -57,18 +38,32 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+    buildFeatures {
+        buildConfig = true
+        compose = true
     }
-
-    kotlinOptions { jvmTarget = "1.8" }
-    buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.8" }
+    kotlinOptions { jvmTarget = "11" }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes.add("**/**.version")
+            excludes.add("**/**.properties")
+            excludes.add("**/DEPENDENCIES")
+            excludes.add("**/DEPENDENCIES.txt")
+            excludes.add("**/dependencies.txt")
+            excludes.add("**/LICENSE")
+            excludes.add("**/LICENSE.txt")
+            excludes.add("**/license.txt")
+            excludes.add("**/NOTICE")
+            excludes.add("**/NOTICE.txt")
+            excludes.add("**/notice.txt")
+            excludes.add("**/AL2.0")
+            excludes.add("**/LGPL2.1")
         }
     }
 
@@ -78,35 +73,33 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    implementation("androidx.compose.ui:ui:1.5.1")
-    implementation("androidx.tv:tv-foundation:1.0.0-alpha09")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha05")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
-    implementation("io.insert-koin:koin-android:3.4.2")
-    implementation("io.insert-koin:koin-androidx-compose:3.4.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+
+    implementation("androidx.compose.material3:material3:1.2.0-alpha12")
+    implementation("androidx.tv:tv-foundation:1.0.0-alpha10")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui:1.5.4")
+
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("io.insert-koin:koin-android:3.5.3")
+    implementation("io.insert-koin:koin-androidx-compose:3.5.3")
+
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     implementation("com.github.rumboalla.KryptoPrefs:kryptoprefs:0.4.3")
     implementation("com.github.rumboalla.KryptoPrefs:kryptoprefs-gson:0.4.3")
-    implementation("org.jsoup:jsoup:1.16.1")
-    implementation("com.github.topjohnwu.libsu:core:5.2.1")
-    implementation("io.github.g00fy2:versioncompare:1.5.0")
+    implementation("org.jsoup:jsoup:1.17.2")
+    //implementation("com.github.topjohnwu.libsu:core:5.2.1")
+    //implementation("io.github.g00fy2:versioncompare:1.5.0")
 
-    testImplementation("junit:junit:4.13.2")
-
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
-
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.1")
-
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
 }
